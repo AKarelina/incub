0866f76dc9c7701042c7922b6a1a7610f53bda89
AKarelina <annagjindj@gmail.com>
2022-11-01 17:29:53 -0400

    Solution: 52

---
+++ 52.js
@@ -0,0 +1,21 @@
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  oopsGlobal = 5;
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
