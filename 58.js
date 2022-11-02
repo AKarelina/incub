db8fef145295232f68f15d591ef70c487426e6b0
AKarelina <annagjindj@gmail.com>
2022-11-01 23:23:33 -0400

    Solution: 58

--- 58.js
+++ 58.js
@@ -4,4 +4,12 @@ function welcomeToBooleans() {
   return true; // Change this line
 
   // Only change code above this line
+}function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue === true) {
+  return "Yes, that was true";
+}
+return "No, that was false"
+  // Only change code above this line
+
 }
